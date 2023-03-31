63b0ff84976eead9a01f0127533e38c8fab4b8c0
Riitta Pessi <riitta@pessi.fi>
2023-03-31 10:56:04 +0300

    Varmuuskopiointi, gui:n alku talteen

--- dokumentaatio/tuntikirjanpito.md
+++ dokumentaatio/tuntikirjanpito.md
@@ -6,4 +6,5 @@
 | 17.3. | 1      | Sovelluskehitys | Tietokantarakenteen suunnittelua |
 | 25.3. | 1,25   | Dokumentaatio   | Alustava vaatimusmäärittelydokumentti |
 | 25.3. | 1,5    | Sovelluskehitys | TkInteriin tutustumista |
+| 29.3. | 5,25   | Sovelluekehitys | Käyttöliittymän rakentelua |
  
---
+++ src/ui/gui_files.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+from time import sleep
+
+class FilesGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.root.geometry("400x400") #width x height
+        self.root.title("Talouskatsaus")
+        self.label = tk.Label(self.root, text="Valitse talletettava CSV-tiedosto", font=('Arial', 18))
+        self.label.pack(padx=20, pady=20)
+
+        self.buttonframe = tk.Frame(self.root)
+        self.buttonframe.columnconfigure(1,weight = 1)
+        self.buttonframe.columnconfigure(2,weight = 1)
+        self.buttonframe.columnconfigure(3,weight = 1)
+
+        self.btn1 = tk.Button(self.buttonframe, text = "Valitse CSV-tiedosto", font=('Arial', 18), pady = 10, command = self.select_file)
+        self.btn1.grid(row = 0, column = 1)
+        self.btn2 = tk.Button(self.buttonframe, text = "Siirry kyselyihin", font=('Arial', 18), pady = 10, command = self.make_queries)
+        self.btn2.grid(row = 1, column = 1)
+        self.btn3 = tk.Button(self.buttonframe, text = "Lopeta", font=('Arial', 18), pady = 10, command = self.exit)
+        self.btn3.grid(row = 2, column = 1)
+
+        self.buttonframe.pack(fill='x')
+
+        self.root.mainloop()
+
+    def select_file(self):
+        #self.filetypes = (('csv files', '*.csv')), en saanu tätä toimimaan
+        self.file = fd.askopenfilename(initialdir = "/home", title = "Valitse CSV-tiedosto") 
+        print("Otettiin vastaan tiedosto", self.file) #luettavan tiedoston osoite
+    
+    def make_queries(self):
+        print("Siirrytään kyselyihin")
+
+    def exit(self):
+        print("Lopetetaan")
+        sleep(0.5)
+        exit()
+
+FilesGUI()
