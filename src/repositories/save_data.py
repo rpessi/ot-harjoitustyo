56e8bf560f0adcfabfb23d3f5569be7c511ffd34
Riitta Pessi <riitta@pessi.fi>
2023-04-26 16:47:50 +0300

    JSON-tallennus talteen

--- src/repositories/save_data.py
+++ src/repositories/save_data.py
@@ -31,10 +31,38 @@ def save_account(account, test = False):
         data_file_path = os.path.join(dirname, "account_data.csv")
         with open(data_file_path, "a", encoding = "utf8") as writefile:
             writefile.writelines(new_lines)
+        save_to_json(new_lines, account)
         print("Tiedot on tallennettu.")
     else:
         dirname = os.path.dirname(__file__)
         data_file_path = os.path.join(dirname, "../tests", "test_account_data.csv")
         with open(data_file_path, "w", encoding = "utf8") as writefile:
-            writefile.writelines(new_lines)
+            writefile.writelines(new_lines, account)
     return True
+
+def save_to_json(data, account):
+    events = {}
+    for rivi in data:
+        osat = rivi.split(";")
+        if osat[0] not in events:
+            events[osat[0]] = []
+        events[osat[0]].append({"Vuosi": osat[1][:4], "Kk": osat[1][5:7], 
+            "Summa": osat[2], "Nimi": osat[3], "Luokka": osat[4].replace("\n", ""),
+            "Alaluokka": ""})
+    json_string = json.dumps(events, indent = 2)
+    name = account.name + ".json"
+    with open(name, 'w') as file:
+        file.write(json_string) #tallentaa repon juureen
+
+def read_from_json(name, criteria, search):
+    filename = name + ".json"
+    with open(filename, 'r') as file:
+        events = json.loads(file.read())
+        total = 0
+    for event in events[name]:
+        if event[criteria] == search:
+            print(f"{search}  {event['Summa']}")
+            total += float(event['Summa'])
+    print(f"Yhteensä {round(total, 2)}")
+
+    
