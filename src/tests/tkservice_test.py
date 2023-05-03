31b2e479735f104a792d8067a4667287d544f6e2
Riitta Pessi <riitta@pessi.fi>
2023-05-03 21:56:23 +0300

    Testejä laajennettu

--- src/tests/tkservice_test.py
+++ src/tests/tkservice_test.py
@@ -1,6 +1,7 @@
 import unittest
 from services.tk_service import TKService
-from repositories.save_data import process_account
+from repositories.save_data import process_account, get_account_names, read_from_json, combine_to_json
+from repositories.save_data import combine_to_json
 import os
 from config import CSV_FILENAME, ACCOUNTS_FILENAME
 
@@ -14,6 +15,10 @@ class TestTKService(unittest.TestCase):
             os.remove(os.path.join(dirname, CSV_FILENAME))
         if os.path.exists(os.path.join(dirname, "../tests/TEST.json")):
             os.remove(os.path.join(dirname, "../tests/TEST.json"))
+        if os.path.exists(os.path.join(dirname, "../tests/TEST2.json")):
+            os.remove(os.path.join(dirname, "../tests/TEST2.json"))
+        if os.path.exists(os.path.join(dirname, "../tests/UNITED.json")):
+            os.remove(os.path.join(dirname, "../tests/UNITED.json"))
         self.account = TKService("TEST")
         self.account.interests["NORDEA LAINAT 1"] = -500.0
         self.account.interests["NORDEA LAINAT 2"] = -1500.0
@@ -28,6 +33,11 @@ class TestTKService(unittest.TestCase):
         self.account.offset_account_out['NORDEA RAHOITUS SUOMI OY'] = 'Menot'
         self.account.offset_account_out['IF VAKUUTUS'] = 'Menot'
         self.account.offset_account_out['Elisa Oyj'] = 'Menot'
+        self.account2 = TKService("TEST2")
+        self.data2 = os.path.join(dirname, "Nordea2.csv")
+        self.account2.offset_account_in['PALKKA'] = 'Tulot'
+        self.account2.offset_account_out['Elisa Oyj'] = 'Menot'
+
 
     def test_summary_counts_money_out_correctly(self):
         self.account.summary(self.data)
@@ -86,3 +96,38 @@ class TestTKService(unittest.TestCase):
     def test_process_account_works(self):
         result = process_account(self.account, self.data)
         self.assertEqual(result, True)
+
+    def test_get_account_names_works(self):
+        process_account(self.account, self.data)
+        result = get_account_names()
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0], "TEST")
+
+    def test_read_from_json_works(self):
+        process_account(self.account, self.data)
+        result = read_from_json(self.account.name, 'Nimi', 'elisa')
+        self.assertEqual(result, -539.74)
+        result = read_from_json(self.account.name, 'Nimi', 'asun')
+        self.assertEqual(result, -5796.3) 
+
+    def test_combine_to_json_works(self):
+        result0 = combine_to_json([], "UNITED")
+        self.assertEqual(result0, [])
+        process_account(self.account, self.data)
+        process_account(self.account2, self.data2)
+        combine_to_json(["TEST", "TEST2"], "UNITED")
+        result1 = get_account_names()
+        self.assertEqual(result1, ['TEST', 'TEST2', 'UNITED'])
+        result2, result3 = read_from_json('UNITED', 'Nimi', 'eli'), read_from_json('UNITED', 'Nimi', 'alt')
+        self.assertEqual(result2, -999.63)
+        self.assertEqual(result3, 0)
+
+
+
+
+
+
+
+        
+
+
